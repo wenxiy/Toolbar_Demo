c9538fdb8cd6d488ea6bd42a7d818071f951c27b
wenxiy <193367189@qq.com>
2020-07-04 13:07:08 +0800

    debug

--- app/src/main/java/com/example/toolbar_demo/activity/MainActivity.java
+++ app/src/main/java/com/example/toolbar_demo/activity/MainActivity.java
@@ -1,13 +1,21 @@
 package com.example.toolbar_demo.activity;
 
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.toolbar_demo.R;
 import com.example.toolbar_demo.adapter.RecyclerViewadapter;
@@ -17,7 +25,8 @@ public class MainActivity extends AppCompatActivity {
     private Toolbar mtoolbar;
     private RecyclerViewadapter viewadapter;
     private LinearLayoutManager linearLayoutManager;
-
+    private DrawerLayout mdrawerlayout;
+    private TextView tv_clove;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,20 +34,49 @@ public class MainActivity extends AppCompatActivity {
         initview();
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu,menu);
+        return true;
+    }
+
     public void initview() {
-        mrecyclerview = findViewById(R.id.recyclerview);
+      //  mrecyclerview = findViewById(R.id.recyclerview);
         mtoolbar = findViewById(R.id.toolbar);
         mtoolbar.setTitle("Toolbar");
+        mtoolbar.setSubtitle("副标题");
+        mtoolbar.setSubtitleTextColor(getResources().getColor(R.color.colorPrimary));
+        mtoolbar.setNavigationIcon(R.mipmap.ic_launcher_round);
         setSupportActionBar(mtoolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setLogo(R.drawable.ic_launcher_background);
         mtoolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
             @Override
             public boolean onMenuItemClick(MenuItem item) {
-                return false;
+                switch (item.getItemId()){
+                    case R.id.action_setting:
+                        Toast.makeText(MainActivity.this,"action_setting",Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.action_share:
+                        Toast.makeText(MainActivity.this, "action_share", Toast.LENGTH_SHORT).show();
+                        break;
+                    default:
+                        break;
+                }
+                return true;
             }
         });
-
+        mdrawerlayout=findViewById(R.id.id_drawerlayout);
+        ActionBarDrawerToggle mdrawertoggle = new ActionBarDrawerToggle(this, mdrawerlayout, mtoolbar, R.string.drawer_close, R.string.drawer__open);
+        mdrawertoggle.syncState();
+        mdrawerlayout.addDrawerListener(mdrawertoggle);
+        tv_clove=findViewById(R.id.tv_close);
+        tv_clove.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mdrawerlayout.closeDrawer(Gravity.LEFT);
+            }
+        });
     }
 
 }
